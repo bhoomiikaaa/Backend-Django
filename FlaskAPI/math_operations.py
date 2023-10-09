c60fa7afccff0f9499481839b25860546c2911f2
Bhoomika Singh <90466055+bhoomiikaaa@users.noreply.github.com>
2023-10-09 20:48:32 +0530

    Add files via upload

---
+++ FlaskAPI/math_operations.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+if __name__ == "__main__":
+    print("Addition:", add(1, 2))
+    print("Subtraction:", subtract(10, 5))
+    print("Multiplication:", multiply(3, 4))
+    print("Division:", divide(12, 6))
